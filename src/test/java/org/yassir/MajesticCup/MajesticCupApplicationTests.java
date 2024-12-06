ed29e34db3f8b0766cf329845b4a420413116f8f
yassir5236 <yassiraitlahmadi@gmail.com>
2024-12-06 11:11:02 +0100

    first commit

---
+++ src/test/java/org/yassir/MajesticCup/MajesticCupApplicationTests.java
@@ -0,0 +1,13 @@
+package org.yassir.MajesticCup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MajesticCupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
