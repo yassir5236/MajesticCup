24e49f41e34f97a133a82ab90ef9d2d44b7fea7f
yassir5236 <yassiraitlahmadi@gmail.com>
2024-12-07 14:11:24 +0100

    adding player,team entity

--- src/main/java/org/yassir/MajesticCup/model/entity/Player.java
+++ src/main/java/org/yassir/MajesticCup/model/entity/Player.java
@@ -1,4 +1,15 @@
 package org.yassir.MajesticCup.model.entity;
 
+import lombok.*;
+
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class Player {
+    private String id;
+    private String name;
+    private String surname;
+    private String position;
+    private int number;
 }
